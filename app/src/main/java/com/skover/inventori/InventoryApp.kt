d545ce98bdb749525641c3a642b6c890c124d20d
Skover3563 <kostiaostrouhov6e@gmail.com>
2024-10-22 20:51:54 +0300

    Оставил только экраны Inventori

--- app/src/main/java/com/skover/inventori/InventoryApp.kt
+++ app/src/main/java/com/skover/inventori/InventoryApp.kt
@@ -1,29 +1,19 @@
 package com.skover.inventori
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -42,7 +32,7 @@ fun InventoryApp() {
             modifier = Modifier.padding(padding)
 
         ) {
-            composable(INVETORi_SCREEN) { CityiScreen(navController) }
+            composable(INVETORi_SCREEN) { InventoriScreen(navController) }
             composable(ADD_ITEM_SCREEN) { AddItemScreen(navController) }
             composable(EDIT_ITEM_SCREEN) { EditItemScreen(navController) }
             composable(ITEM_DETALIC_SCREEN) { ItemDetalicScreen(navController) }
@@ -50,30 +40,25 @@ fun InventoryApp() {
         }
     }
 }
+
+@Composable
+fun InventoriScreen(navController: NavHostController) {
+
+}
+
 @Composable
 fun AddItemScreen(navController: NavHostController) {
-    var name by rememberSaveable { mutableStateOf("") }
-    var surname by rememberSaveable { mutableStateOf("") }
-    var resualt by rememberSaveable { mutableStateOf("") }
-    Column {
-        TextField(value = name, onValueChange = { name = it })
-        TextField(value = surname, onValueChange = { surname = it })
-        Button(onClick = { resualt =  "$name $surname "  }) {
-            Text(text = "Aнука что там")
-        }
-
-    }
 
 }
 
 @Composable
 fun EditItemScreen(navController: NavHostController) {
-    TODO("Not yet implemented")
+
 }
 
 @Composable
 fun ItemDetalicScreen(navController: NavHostController) {
-    TODO("Not yet implemented")
+
 }
 
 
